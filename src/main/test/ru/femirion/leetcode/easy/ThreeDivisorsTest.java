69ed8d465c8326ed3500e9732577628488ddec0f
Aleksandr Vostryakov <femirion@gmail.com>
2023-10-31 18:31:57 +0300

    1952. Three Divisors

---
+++ src/main/test/ru/femirion/leetcode/easy/ThreeDivisorsTest.java
@@ -0,0 +1,16 @@
+package ru.femirion.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ThreeDivisorsTest {
+    @Test
+    void isThree() {
+        // given
+        ThreeDivisors divisors = new ThreeDivisors();
+
+        // then
+        assertThat(divisors.isThree(4)).isEqualTo(true);
+    }
+}
