9483726375215ee67c8a7b63eba80466838ec3bf
Aleksandr Vostryakov <femirion@gmail.com>
2023-11-16 12:45:50 +0300

    922. Sort Array By Parity II

---
+++ src/main/java/ru/femirion/leetcode/easy/SortArrayByParityII.java
@@ -0,0 +1,74 @@
+package ru.femirion.leetcode.easy;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 922. Sort Array By Parity II
+ * https://leetcode.com/problems/sort-array-by-parity-ii/
+ */
+public class SortArrayByParityII {
+
+    /**
+     * Runtime 22ms Beats 5.18% of users with Java
+     * Memory 45.27MB Beats 22.75% of users with Java
+     */
+    public int[] sortArrayByParityII(int[] nums) {
+        PriorityQueue<Integer> oddQueue = new PriorityQueue<>(Comparator.naturalOrder());
+        PriorityQueue<Integer> evenQueue = new PriorityQueue<>(Comparator.naturalOrder());
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                evenQueue.offer(nums[i]);
+            } else {
+                oddQueue.offer(nums[i]);
+            }
+        }
+
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                result[i] = evenQueue.remove();
+            } else {
+                result[i] = oddQueue.remove();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Runtime 3ms Beats 45.62% of users with Java
+     * Memory 46.38MB Beats 5.90% of users with Java
+     */
+    public int[] sortArrayByParityII1(int[] nums) {
+        int[] oddQueue = new int[nums.length/2];
+        int[] evenQueue = new int[nums.length/2];
+
+        int oddPointer = 0;
+        int evenPointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                evenQueue[evenPointer] = nums[i];
+                evenPointer++;
+            } else {
+                oddQueue[oddPointer] = nums[i];
+                oddPointer++;
+            }
+        }
+
+        oddPointer = 0;
+        evenPointer = 0;
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                result[i] = evenQueue[evenPointer];
+                evenPointer++;
+            } else {
+                result[i] = oddQueue[oddPointer];
+                oddPointer++;
+            }
+        }
+        return result;
+    }
+
+}
