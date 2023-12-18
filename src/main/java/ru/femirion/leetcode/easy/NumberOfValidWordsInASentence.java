a41716627f5f1cf91b891bb5a70290600ce87efc
Aleksandr Vostryakov <femirion@gmail.com>
2023-12-18 14:56:47 +0300

    2047. Number of Valid Words in a Sentence

---
+++ src/main/java/ru/femirion/leetcode/easy/NumberOfValidWordsInASentence.java
@@ -0,0 +1,69 @@
+package ru.femirion.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2047. Number of Valid Words in a Sentence
+ * https://leetcode.com/problems/number-of-valid-words-in-a-sentence/
+ */
+public class NumberOfValidWordsInASentence {
+
+    /**
+     * Runtime 4ms Beats 78.34%of users with Java
+     * Memory 43.66MB Beats 58.12%of users with Java
+     */
+    public int countValidWords(String sentence) {
+        String[] words = sentence.split(" ");
+        int count = 0;
+        List<String> w = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (isValid(words[i], w)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isValid(String str, List<String> w) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        int countOfHyphen = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            if (isDigit(current)) {
+                return false;
+            }
+            if (current == '-') {
+                if (i == 0 || i == str.length() - 1 || countOfHyphen == 1
+                        || !isLetter(str.charAt(i - 1)) || !isLetter(str.charAt(i + 1))) {
+                    return false;
+                } else {
+                    countOfHyphen++;
+                }
+                continue;
+            }
+            if (isPunctuation(current)) {
+                if (i != str.length() - 1) {
+                    return false;
+                }
+            }
+        }
+        w.add(str);
+        return true;
+    }
+
+    private boolean isDigit(char current) {
+        return current >= '0' && current <= '9';
+    }
+
+    private boolean isPunctuation(char current) {
+        return current == ',' || current == '.' || current == '!';
+    }
+
+    private boolean isLetter(char current) {
+        return current >= 'a' && current <= 'z';
+    }
+}
+
