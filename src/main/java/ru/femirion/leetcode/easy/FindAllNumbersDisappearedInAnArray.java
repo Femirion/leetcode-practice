77f1d279a488be5a2f6fb0079ac69e17389e3172
Aleksandr Vostryakov <femirion@gmail.com>
2024-02-02 13:40:59 +0300

    448. Find All Numbers Disappeared in an Array

--- src/main/java/ru/femirion/leetcode/easy/FindAllNumbersDisappearedInAnArray.java
+++ src/main/java/ru/femirion/leetcode/easy/FindAllNumbersDisappearedInAnArray.java
@@ -27,4 +27,22 @@ public class FindAllNumbersDisappearedInAnArray {
         }
         return new ArrayList<>(set);
     }
+
+    /**
+     * Runtime 19 ms Beats 17.61% of users with Java
+     * Memory 55.25 MB Beats 19.52% of users with Java
+     */
+    public List<Integer> findDisappearedNumbers1(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        for (int i = 1; i < nums.length + 1; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
 }
