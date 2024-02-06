d6c2ac914050378dd2cf17f5929fa6463f71329f
Aleksandr Vostryakov <femirion@gmail.com>
2024-02-06 16:12:27 +0300

    49. Group Anagrams

--- src/main/java/ru/femirion/leetcode/medium/StringWithoutRepeatingCharacters.java
+++ src/main/java/ru/femirion/leetcode/medium/StringWithoutRepeatingCharacters.java
@@ -3,41 +3,17 @@ package ru.femirion.leetcode.medium;
 import java.util.Arrays;
 import java.util.HashSet;
 
-/*
-    https://leetcode.com/problems/longest-substring-without-repeating-characters/
-
-    Given a string s, find the length of the longest substring without repeating characters.
-
-    Example 1:
-    Input: s = "abcabcbb"
-    Output: 3
-    Explanation: The answer is "abc", with the length of 3.
-
-    Example 2:
-    Input: s = "bbbbb"
-    Output: 1
-    Explanation: The answer is "b", with the length of 1.
-
-    Example 3:
-    Input: s = "pwwkew"
-    Output: 3
-    Explanation: The answer is "wke", with the length of 3.
-    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
-    Example 4:
-    Input: s = ""
-    Output: 0
-
-    Constraints:
-    0 <= s.length <= 5 * 104
-    s consists of English letters, digits, symbols and spaces.*/
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ */
 public class StringWithoutRepeatingCharacters {
 
-    /*
+    /**
      * Runtime: 73 ms, faster than 16.95% of Java online submissions for Longest Substring Without Repeating Characters.
      * Memory Usage: 40 MB, less than 14.14% of Java online submissions for Longest Substring Without Repeating Characters.
      *
-     * */
+     */
     public int lengthOfLongestSubstring(String s) {
         char[] chars = s.toCharArray();
         int max = 0;
