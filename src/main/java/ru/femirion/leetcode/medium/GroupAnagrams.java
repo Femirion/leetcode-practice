544fcc32ab0d3270d74060216c54bb3a94f963e6
Aleksandr Vostryakov <femirion@gmail.com>
2024-02-06 14:02:11 +0300

    49. Group Anagrams

--- src/main/java/ru/femirion/leetcode/medium/GroupAnagrams.java
+++ src/main/java/ru/femirion/leetcode/medium/GroupAnagrams.java
@@ -1,114 +1,28 @@
 package ru.femirion.leetcode.medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * https://leetcode.com/problems/group-anagrams/
- * <p>
- * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
- * <p>
- * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
- * typically using all the original letters exactly once.
- * <p>
- * <p>
- * <p>
- * Example 1:
- * <p>
- * Input: strs = ["eat","tea","tan","ate","nat","bat"]
- * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
- * Example 2:
- * <p>
- * Input: strs = [""]
- * Output: [[""]]
- * Example 3:
- * <p>
- * Input: strs = ["a"]
- * Output: [["a"]]
- * <p>
- * <p>
- * Constraints:
- * <p>
- * 1 <= strs.length <= 104
- * 0 <= strs[i].length <= 100
- * strs[i] consists of lowercase English letters.
+ * 49. Group Anagrams
+ * https://leetcode.com/problems/group-anagrams/description/?envType=daily-question&envId=2024-02-06
  */
 public class GroupAnagrams {
 
+    /**
+     * Runtime 7 ms Beats 57.75% of users with Java
+     * Memory 47.31 MB Beats 74.71% of users with Java
+     */
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<List<Integer>, List<String>> anagrams = new HashMap<>();
-
-        for (String str : strs) {
-            List<Integer> chars = new ArrayList<>();
-
-            for (int i = 0; i < str.length(); i++) {
-                char current = str.charAt(i);
-                int currentPosition = getPosition(current);
-
-            }
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String tmp = new String(arr);
+            List<String> list = map.getOrDefault(tmp, new ArrayList<>());
+            list.add(strs[i]);
+            map.put(tmp, list);
         }
-        return null;
+
+        return new ArrayList<>(map.values());
     }
-
-    private int getPosition(char currentChar) {
-        switch (currentChar) {
-            case 'a':
-                return 0;
-            case 'b':
-                return 1;
-            case 'c':
-                return 2;
-            case 'd':
-                return 3;
-            case 'e':
-                return 4;
-            case 'f':
-                return 5;
-            case 'g':
-                return 6;
-            case 'h':
-                return 7;
-            case 'i':
-                return 8;
-            case 'j':
-                return 9;
-            case 'k':
-                return 10;
-            case 'l':
-                return 11;
-            case 'm':
-                return 12;
-            case 'n':
-                return 13;
-            case 'o':
-                return 14;
-            case 'p':
-                return 15;
-            case 'q':
-                return 16;
-            case 'r':
-                return 17;
-            case 's':
-                return 18;
-            case 't':
-                return 19;
-            case 'u':
-                return 20;
-            case 'v':
-                return 21;
-            case 'w':
-                return 22;
-            case 'x':
-                return 23;
-            case 'y':
-                return 24;
-            case 'z':
-                return 25;
-
-        }
-        return -1;
-    }
-
 }
