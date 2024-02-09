0d047866ed471c112edbaf5d5929cf54b9e0a0f2
Aleksandr Vostryakov <femirion@gmail.com>
2024-02-09 14:15:37 +0300

    2235. Add Two Integers

---
+++ src/main/java/ru/femirion/leetcode/easy/AddTwoIntegers.java
@@ -0,0 +1,15 @@
+package ru.femirion.leetcode.easy;
+
+/**
+ * 2235. Add Two Integers
+ * https://leetcode.com/problems/add-two-integers/description/
+ */
+public class AddTwoIntegers {
+    /**
+     * Runtime 0 ms Beats 100.00% of users with Java
+     * Memory 40.10 MB Beats 83.37% of users with Java
+     */
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
