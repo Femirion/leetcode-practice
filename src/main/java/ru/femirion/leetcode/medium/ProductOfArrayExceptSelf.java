690faf715d7903af08797f1499b52665078854c5
Aleksandr Vostryakov <femirion@gmail.com>
2024-02-21 18:59:01 +0300

    238. Product of Array Except Self

--- src/main/java/ru/femirion/leetcode/medium/ProductOfArrayExceptSelf.java
+++ src/main/java/ru/femirion/leetcode/medium/ProductOfArrayExceptSelf.java
@@ -6,7 +6,26 @@ package ru.femirion.leetcode.medium;
  */
 public class ProductOfArrayExceptSelf {
 
+    /**
+     * Runtime 3 ms Beats 20.23% of users with Java
+     * Memory 53.12 MB Beats 71.42% of users with Java
+     */
     public int[] productExceptSelf(int[] nums) {
-        return new int[2];
+        int[] result = new int[nums.length];
+
+        int product = 1;
+        result[0] = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            product = product * nums[i];
+            result[i + 1] = product;
+        }
+
+        product = 1;
+        for (int i = nums.length - 1; 0 < i; i--) {
+            product = product * nums[i];
+            result[i - 1] = product * result[i - 1];
+        }
+
+        return result;
     }
 }
