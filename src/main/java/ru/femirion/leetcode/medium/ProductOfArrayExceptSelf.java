c1af460f9c3e2c5e9edb7bcce10128f9d0445c4e
Aleksandr Vostryakov <femirion@gmail.com>
2024-02-21 13:56:27 +0300

    238. Product of Array Except Self

---
+++ src/main/java/ru/femirion/leetcode/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,12 @@
+package ru.femirion.leetcode.medium;
+
+/**
+ * 238. Product of Array Except Self
+ * https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
+ */
+public class ProductOfArrayExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        return new int[2];
+    }
+}
