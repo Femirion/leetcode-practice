11161c43c3af6bd057183fe29fc648533a196241
Aleksandr Vostryakov <femirion@gmail.com>
2024-03-21 16:19:56 +0300

    2932. Maximum Strong Pair XOR I

---
+++ src/main/java/ru/femirion/leetcode/easy/MaximumStrongPairXORI.java
@@ -0,0 +1,27 @@
+package ru.femirion.leetcode.easy;
+
+/**
+ * 2932. Maximum Strong Pair XOR I
+ * https://leetcode.com/problems/maximum-strong-pair-xor-i/description/
+ */
+public class MaximumStrongPairXORI {
+
+    /**
+     * Runtime 2 ms Beats 95.32% of users with Java
+     * Memory 42.53 MB Beat 94.93% of users with Java
+     */
+    public int maximumStrongPairXor(int[] nums) {
+        int maxXor = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) <= Math.min(nums[i], nums[j])) {
+                    int xor = nums[i] ^ nums[j];
+                    if (xor > maxXor) {
+                        maxXor = xor;
+                    }
+                }
+            }
+        }
+        return maxXor;
+    }
+}
