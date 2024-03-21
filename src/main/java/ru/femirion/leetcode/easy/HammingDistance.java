e1ba2e2f4991f279499617f3dffc7b029720a1e1
Aleksandr Vostryakov <femirion@gmail.com>
2024-03-21 16:24:31 +0300

    461. Hamming Distance

---
+++ src/main/java/ru/femirion/leetcode/easy/HammingDistance.java
@@ -0,0 +1,26 @@
+package ru.femirion.leetcode.easy;
+
+/**
+ * 461. Hamming Distance
+ * https://leetcode.com/problems/hamming-distance/description/
+ */
+public class HammingDistance {
+
+    /**
+     * Runtime 0 ms Beats 100.00% of users with Java
+     * Memory 40.12 MB Beats 78.69% of users with Java
+     */
+    public int hammingDistance(int x, int y) {
+        int result = 0;
+
+        while (0 < x || 0 < y) {
+            if ((x & 1) != (y & 1)) {
+                result++;
+            }
+            x = x >> 1;
+            y = y >> 1;
+        }
+
+        return result;
+    }
+}
