df5837d9932ac31c2d8190a3cd7a65d6a32ea873
Aleksandr Vostryakov <femirion@gmail.com>
2024-03-21 14:06:45 +0300

    190. Reverse Bits

---
+++ src/main/java/ru/femirion/leetcode/easy/ReverseBits.java
@@ -0,0 +1,22 @@
+package ru.femirion.leetcode.easy;
+
+/**
+ * 190. Reverse Bits
+ * https://leetcode.com/problems/reverse-bits/description/
+ */
+public class ReverseBits {
+
+    /**
+     * Runtime 0  ms Beats 100.00% of users with Java
+     * Memory 41.60 MB Beats 57.87% of users with Java
+     */
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans = ans << 1;
+            ans = ans | (n & 1);
+            n = n >> 1;
+        }
+        return ans;
+    }
+}
