a83ea1fb1b9184a1806385fd67c33f5938513363
Aleksandr Vostryakov <femirion@gmail.com>
2024-03-27 16:16:16 +0300

    1624. Largest Substring Between Two Equal Characters

--- src/main/java/ru/femirion/leetcode/easy/LargestSubstringBetweenTwoEqualCharacters.java
+++ src/main/java/ru/femirion/leetcode/easy/LargestSubstringBetweenTwoEqualCharacters.java
@@ -7,6 +7,26 @@ package ru.femirion.leetcode.easy;
 public class LargestSubstringBetweenTwoEqualCharacters {
 
     /**
+     * Runtime 0 ms Beats 100.00% of users with Java
+     * Memory 41.10 MB Beats 86.39% of users with Java
+     */
+    public int maxLengthBetweenEqualCharacters1(String s) {
+        int[] arr = new int[26];
+
+        int max = -1;
+        for (int i = 0; i < s.length(); i++) {
+            int cur = s.charAt(i) - 'a';
+            if (arr[cur] == 0) {
+                arr[cur] = i + 1;
+            } else {
+                max = Math.max(max, i - arr[cur]);
+            }
+        }
+
+        return max;
+    }
+
+    /**
      * Runtime 1 ms Beats 88.34% of users with Java
      * Memory 41.22 MB Beats 60.63% of users with Java
      */
@@ -17,9 +37,9 @@ public class LargestSubstringBetweenTwoEqualCharacters {
         for (int i = 0; i < s.length(); i++) {
             int cur = s.charAt(i) - 'a';
             if (arr[cur][0] == 0) {
-                arr[cur][0] = i;
+                arr[cur][0] = i + 1;
             } else {
-                arr[cur][1] = i;
+                arr[cur][1] = i + 1;
                 int tmp = arr[cur][1] - arr[cur][0];
                 if (max < tmp) {
                     max = tmp;
@@ -27,6 +47,6 @@ public class LargestSubstringBetweenTwoEqualCharacters {
             }
         }
 
-        return max == 0 ? - 1: max;
+        return max - 1;
     }
 }
