96fbcd0d8c557c9715c064f4e15605342a52fb4a
Aleksandr Vostryakov <femirion@gmail.com>
2024-04-09 17:58:45 +0300

    7. Reverse Integer

--- src/main/java/ru/femirion/leetcode/medium/ReverseInteger.java
+++ src/main/java/ru/femirion/leetcode/medium/ReverseInteger.java
@@ -7,6 +7,26 @@ package ru.femirion.leetcode.medium;
 public class ReverseInteger {
 
     /**
+     * Runtime 0 ms Beats 100.00% of users with Java
+     * Memory 40.11 MB Beats 98.50% of users with Java
+     */
+    public int reverse1(int x) {
+        long result = 0;
+
+        while (x != 0) {
+            if (result * 10 > Integer.MAX_VALUE || result * 10 < Integer.MIN_VALUE) {
+                return 0;
+            }
+
+            int cur = x % 10;
+            result = result * 10 + cur;
+            x = x / 10;
+        }
+
+        return (int) result;
+    }
+
+    /**
      * Runtime 2 ms Beats 19.43%  of users with Java
      * Memory 41.08  MB  Beats 25.42% of users with Java
      */
