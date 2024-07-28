9f709a36d995c9d12d294335c5cad1f7fc90cabf
Aleksandr Vostryakov <femirion@gmail.com>
2024-07-28 23:44:27 +0300

    2657. Find the Prefix Common Array of Two Arrays

--- src/main/java/ru/femirion/leetcode/medium/FindThePrefixCommonArrayOfTwoArrays.java
+++ src/main/java/ru/femirion/leetcode/medium/FindThePrefixCommonArrayOfTwoArrays.java
@@ -10,6 +10,35 @@ import java.util.Set;
 public class FindThePrefixCommonArrayOfTwoArrays {
 
     /**
+     * Runtime 2 ms Beats 93.60%
+     * Memory 45.40 MB Beats 58.99%
+     */
+    public int[] findThePrefixCommonArray2(int[] A, int[] B) {
+        int[] setArr1 = new int[A.length];
+        int[] setArr2 = new int[B.length];
+
+        int[] result = new int[A.length];
+        int prefixSize = 0;
+        for(int i = 0; i < A.length; i++) {
+            if (A[i] == B[i]) {
+                prefixSize++;
+
+            } else {
+                if (setArr1[B[i] - 1] == 1) {
+                    prefixSize++;
+                }
+                if (setArr2[A[i] - 1] == 1) {
+                    prefixSize++;
+                }
+            }
+            result[i] = prefixSize;
+            setArr1[A[i] - 1] = 1;
+            setArr2[B[i] - 1] = 1;
+        }
+        return result;
+    }
+
+    /**
      * Runtime 5 ms Beats 45.96%
      * Memory 45.15 MB Beats 90.00%
      */
