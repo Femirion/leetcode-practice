98da68afe4858cb14cff451e81d69514ed4b4dc7
Aleksandr Vostryakov <femirion@gmail.com>
2024-08-02 17:21:27 +0300

    459. Repeated Substring Pattern

--- src/main/java/ru/femirion/leetcode/easy/RepeatedSubstringPattern.java
+++ src/main/java/ru/femirion/leetcode/easy/RepeatedSubstringPattern.java
@@ -7,6 +7,39 @@ package ru.femirion.leetcode.easy;
 public class RepeatedSubstringPattern {
 
     /**
+     * Runtime 11 ms Beats 86.56%
+     * Memory 44.65 MB Beats 93.17%
+     */
+    public boolean repeatedSubstringPattern2(String s) {
+        for (int i = s.length() / 2; 0 < i; i--) {
+            if (s.length() % i != 0) {
+                continue;
+            }
+            int repeatCount = s.length() / i;
+            int p = 0;
+            int q = p + i;
+            int finishP = q;
+            boolean equalString = true;
+            while (q < s.length()) {
+                int curP = s.charAt(p);
+                int curQ = s.charAt(q);
+                if (curP == curQ) {
+                    q++;
+                    p++;
+                } else {
+                    equalString = false;
+                    break;
+                }
+            }
+            if (equalString) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Runtime 41 ms Beats 56.51% of users with Java
      * Memory 44.87 MB Beats 84.51% of users with Java
      */
