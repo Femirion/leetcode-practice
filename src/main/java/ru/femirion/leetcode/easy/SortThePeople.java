47639d070f6da0702b8498d5fba53322817320a3
Aleksandr Vostryakov <femirion@gmail.com>
2024-08-03 23:06:21 +0300

    2418. Sort the People

--- src/main/java/ru/femirion/leetcode/easy/SortThePeople.java
+++ src/main/java/ru/femirion/leetcode/easy/SortThePeople.java
@@ -1,14 +1,31 @@
 package ru.femirion.leetcode.easy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 2418. Sort the People
  * https://leetcode.com/problems/sort-the-people/description/
  */
 public class SortThePeople {
+
+    /**
+     * Runtime 7 ms Beats 91.55%
+     * Memory 44.99 MB Beats 96.38%
+     */
+    public String[] sortPeople2(String[] names, int[] heights) {
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+        Arrays.sort(heights);
+        String[] result = new String[heights.length];
+        int index = 0;
+        for (int i = heights.length - 1; i >= 0; i--) {
+            result[index] = map.get(heights[i]);
+            index++;
+        }
+        return result;
+    }
 
     /**
      * Runtime 10 ms Beats 28.64%
