f54e5a4acd83945956f7e1fc5be98007589003df
Aleksandr Vostryakov <femirion@gmail.com>
2024-08-04 16:59:57 +0300

    763. Partition Labels

--- src/main/java/ru/femirion/leetcode/medium/PartitionLabels.java
+++ src/main/java/ru/femirion/leetcode/medium/PartitionLabels.java
@@ -12,6 +12,49 @@ import java.util.Map;
 public class PartitionLabels {
 
     /**
+     * Runtime 5 ms Beats 77.39%
+     * Memory 41.94 MB Beats 67.54%
+     */
+    public List<Integer> partitionLabels2(String s) {
+        int[] startPoss = new int[26];
+        int[] endPoss = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int cur = s.charAt(i) - 'a';
+            if (startPoss[cur] == 0) {
+                startPoss[cur] = i;
+            }
+            int curEnd = endPoss[cur];
+            if (curEnd < i) {
+                endPoss[cur] = i;
+            }
+        }
+
+        int start = 0;
+        int end = 0;
+        int startC;
+        int endC;
+        int lengthC = 1;
+        int curLength = 1;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            int cur = s.charAt(i) - 'a';
+            startC = startPoss[cur];
+            endC = endPoss[cur];
+            if (end < endC) {
+                end = endC;
+            }
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+                end = i + 1;
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * Runtime 15 ms Beats 6.85%
      * Memory 42.54 MB Beats 18.92%
      */
