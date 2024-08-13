8f264295c01af310737e08329df7d32a8c42dabd
Aleksandr Vostryakov <femirion@gmail.com>
2024-08-13 12:56:30 +0300

    424. Longest Repeating Character Replacement

--- src/main/java/ru/femirion/leetcode/medium/LongestRepeatingCharacterReplacement.java
+++ src/main/java/ru/femirion/leetcode/medium/LongestRepeatingCharacterReplacement.java
@@ -11,6 +11,37 @@ import java.util.Set;
 public class LongestRepeatingCharacterReplacement {
 
     /**
+     * Runtime 5 ms Beats 98.15%
+     * Memory 42.88 MB Beats 36.05%
+     */
+    public int characterReplacement2(String s, int k) {
+        char[] arr = s.toCharArray();
+        int[] counts = new int[26];
+        int maxCount = 0;
+
+        int maxLength = 0;
+        int L = 0;
+        for (int R = 0; R < arr.length; R++) {
+            counts[arr[R] - 'A']++;
+
+            if (maxCount < counts[arr[R] - 'A']) {
+                maxCount = counts[arr[R] - 'A'];
+            }
+
+            if (k < R - L + 1 - maxCount) {
+                counts[arr[L] - 'A']--;
+                L++;
+            }
+
+            if (maxLength < R - L + 1) {
+                maxLength = R - L + 1;
+            }
+        }
+
+        return maxLength;
+    }
+
+    /**
      * Runtime 28 ms Beats 25.69%
      * Memory 45.38 MB Beats 6.07%
      */
