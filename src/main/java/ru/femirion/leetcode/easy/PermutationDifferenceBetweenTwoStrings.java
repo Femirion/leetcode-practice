39622291a37397cfba3c32e28457bc79550c711a
Aleksandr Vostryakov <femirion@gmail.com>
2024-10-22 13:01:14 +0300

    3146. Permutation Difference between Two Strings

--- src/main/java/ru/femirion/leetcode/easy/PermutationDifferenceBetweenTwoStrings.java
+++ src/main/java/ru/femirion/leetcode/easy/PermutationDifferenceBetweenTwoStrings.java
@@ -10,6 +10,28 @@ import java.util.Map;
 public class PermutationDifferenceBetweenTwoStrings {
 
     /**
+     * Runtime 1 ms Beats 100.00%
+     * Memory 42.61 MB Beats 38.32%
+     */
+    public int findPermutationDifference2(String s, String t) {
+        int[] positions = new int[26];
+
+        int position = 0;
+        for (char c : t.toCharArray()) {
+            positions[c - 'a'] = position;
+            position++;
+        }
+
+        position = 0;
+        int result = 0;
+        for (char c : s.toCharArray()) {
+            result = result + (Math.abs(position - positions[c - 'a']));
+            position++;
+        }
+        return result;
+    }
+
+    /**
      * Runtime 3 ms Beats 12.96%
      * Memory 43.42 MB Beats 6.15%
      */
