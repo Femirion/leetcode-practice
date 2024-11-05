f2c9440f00e0c82132c00286e49b2bd6dc0d0684
Aleksandr Vostryakov <femirion@gmail.com>
2024-11-05 17:28:34 +0300

    2000. Reverse Prefix of Word

--- src/main/java/ru/femirion/leetcode/easy/ReversePrefixOfWord.java
+++ src/main/java/ru/femirion/leetcode/easy/ReversePrefixOfWord.java
@@ -42,4 +42,51 @@ public class ReversePrefixOfWord {
 
         return sb.toString();
     }
+
+    /**
+     * Runtime 1 ms Beats 58.50%
+     * Memory 41.59 MB Beats 65.88%
+     */
+    public String reversePrefix2(String word, char ch) {
+        int i = 0;
+        for (; i < word.length(); i++) {
+            if (ch == word.charAt(i)) {
+                break;
+            }
+        }
+
+        if (i == word.length()) {
+            return word;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int j = i; 0 <= j; j--) {
+            sb.append(word.charAt(j));
+        }
+
+        i++;
+        for (; i < word.length(); i++) {
+            sb.append(word.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Runtime 0 ms Beats 100.00%
+     * Memory 41.39 MB Beats 89.29%
+     */
+    public String reversePrefix3(String word, char ch) {
+        int pos = word.indexOf(ch);
+        if (pos == -1) {
+            return word;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        return sb
+                .append(word, 0, pos + 1)
+                .reverse()
+                .append(word.substring(pos + 1))
+                .toString();
+    }
 }
